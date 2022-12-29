33c9cdbc1484af3b57140cd8380845577a8b9fe9
ylijokic <44416709+ylijokic@users.noreply.github.com>
2022-12-29 10:41:00 -0800

    Add django_rest_framework support

--- notes/api/views.py
+++ notes/api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from .models import Note
+from .serializers import NoteSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         {
@@ -37,4 +41,18 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, id):
+    note = Note.objects.get(id=id)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
